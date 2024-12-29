6f7b06c853c4b664128b688ee4702aea9eee7303
HisyamSamAm <mhisyamnajwan@gmail.com>
2024-12-29 21:10:05 +0700

    Pertemuan TGS 10

---
+++ Pertemuan10/Praktikum/P10_714230055/P10_714230055/view/Form1.cs
@@ -0,0 +1,140 @@
+﻿using P10_714230055.controller;
+using P10_714230055.model;
+using P9_714230055;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace P10_714230055
+{
+    public partial class Form1 : Form
+    {
+        Koneksi koneksi = new Koneksi();
+        M_mahasiswa m_mhs = new M_mahasiswa();
+        public void Tampil()
+        {
+            // Query DB Get MGS
+            DataMahasiswa.DataSource = koneksi.ShowData("SELECT * FROM t_mahasiswa");
+            //Kolom Tabel
+            DataMahasiswa.Columns[0].HeaderText = "NPM";
+            DataMahasiswa.Columns[1].HeaderText = "Nama";
+            DataMahasiswa.Columns[2].HeaderText = "Angkatan";
+            DataMahasiswa.Columns[3].HeaderText = "Alamat";
+            DataMahasiswa.Columns[4].HeaderText = "Email";
+            DataMahasiswa.Columns[5].HeaderText = "No HP";
+
+        }
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //memanggil Tampil
+            Tampil();
+        }
+        public void ResetForm()
+        {
+            textboxNPM.Text = "";
+            textboxNama.Text = "";
+            comboBoxAngkatan.SelectedIndex = -1;
+            textboxAlamat.Text = "";
+            textboxEmail.Text = "";
+            textboxNohp.Text = "";
+        }
+        private void btnSimpan_Click(object sender, EventArgs e)
+        {
+            if (textboxNPM.Text == "" || textboxNama.Text == "" || comboBoxAngkatan.SelectedIndex == -1 || textboxAlamat.Text == "" ||textboxEmail.Text == "" || textboxNohp.Text == "")
+            {
+                MessageBox.Show("Data tidak boleh kosong", "Peringatan",
+               MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            else
+            {
+                Mahasiswa mhs = new Mahasiswa();
+                m_mhs.Npm = textboxNPM.Text;
+                m_mhs.Nama = textboxNama.Text;
+                m_mhs.Angkatan = comboBoxAngkatan.Text;
+                m_mhs.Alamat = textboxAlamat.Text;
+                m_mhs.Email = textboxEmail.Text;
+                m_mhs.Nohp = textboxNohp.Text;
+
+                mhs.Insert(m_mhs);
+
+                ResetForm();
+                Tampil();
+            }
+        }
+
+        private void DataMahasiswa_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            textboxNPM.Text = DataMahasiswa.Rows[e.RowIndex].Cells[0].Value.ToString();
+            textboxNama.Text = DataMahasiswa.Rows[e.RowIndex].Cells[1].Value.ToString();
+            comboBoxAngkatan.Text = DataMahasiswa.Rows[e.RowIndex].Cells[2].Value.ToString();
+            textboxAlamat.Text = DataMahasiswa.Rows[e.RowIndex].Cells[3].Value.ToString();
+            textboxEmail.Text = DataMahasiswa.Rows[e.RowIndex].Cells[4].Value.ToString();
+            textboxNohp.Text = DataMahasiswa.Rows[e.RowIndex].Cells[5].Value.ToString();
+        }
+
+        private void btnUbah_Click(object sender, EventArgs e)
+        {
+            if (textboxNPM.Text == "" || textboxNama.Text == "" || comboBoxAngkatan.SelectedIndex == -1 || textboxAlamat.Text == "" || textboxEmail.Text == "" || textboxNohp.Text == "")
+            {
+                MessageBox.Show("Data tidak boleh kosong", "Peringatan",
+               MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            else
+            {
+                Mahasiswa mhs = new Mahasiswa();
+                m_mhs.Npm = textboxNPM.Text;
+                m_mhs.Nama = textboxNama.Text;
+                m_mhs.Angkatan = comboBoxAngkatan.Text;
+                m_mhs.Alamat = textboxAlamat.Text;
+                m_mhs.Email = textboxEmail.Text;
+                m_mhs.Nohp = textboxNohp.Text;
+
+                mhs.Update(m_mhs, m_mhs.Npm);
+
+                ResetForm();
+                Tampil();
+            }
+        }
+
+        private void btnRefresh_Click(object sender, EventArgs e)
+        {
+            ResetForm();
+            Tampil();
+        }
+
+        private void btnHapus_Click(object sender, EventArgs e)
+        {
+            if (textboxNPM.Text == "" || textboxNama.Text == "" || comboBoxAngkatan.SelectedIndex == -1 || textboxAlamat.Text == "" || textboxEmail.Text == "" || textboxNohp.Text == "")
+            {
+                MessageBox.Show("Data Untuk Menghapus tidak boleh kosong", "Peringatan", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            DialogResult pesan = MessageBox.Show(
+                "Apakah yakin akan menghapus data ini?",
+                "Perhatian",
+                MessageBoxButtons.YesNo,
+                MessageBoxIcon.Question);
+
+            if (pesan == DialogResult.Yes)
+            {
+                Mahasiswa mhs = new Mahasiswa();
+                mhs.Delete(textboxNPM.Text);
+                ResetForm();
+                Tampil();
+            }
+        }
+    }
+}
